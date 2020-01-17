66408b9b46d5e7d5c42d6fe02f35698599d5dd35
abrekos <54232528+abrekos@users.noreply.github.com>
2020-01-17 14:49:58 +0500

    Add files via upload

---
+++ src/script.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         QR sharer
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Добавляет кнопку для показа QR-кода с адресом текущей страницы
+// @author       Abrekos
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const CONFIG = {
+    	scripts: [
+    		''
+    	]
+    }
+
+    loadScript = ( url ) => {
+
+    }
+
+    loadQRLib = () => 
+})();
